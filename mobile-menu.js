bf0cc5564522ddc75a3cc8a8fe401914c68f08a2
somarib <yesomarib1@gmail.com>
2022-11-23 07:48:45 +0000

    Add event Listener to close mobile menu after click on close or any menu item

--- mobile-menu.js
+++ mobile-menu.js
@@ -2,3 +2,9 @@ let btn=document.querySelector("#mbm-icon");
 btn.addEventListener("click",function(){
     document.getElementById("hide-show").style.display="block";
 });
+
+document.querySelectorAll(".mbmenu-li-a").forEach((item)=>{
+    item.addEventListener("click",()=>{
+        document.getElementById("hide-show").style.display="none";
+    })
+});
