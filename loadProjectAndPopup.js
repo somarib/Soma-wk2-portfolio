0a4495cc46d2b6799c2ed84e016f0bda864fcc32
somarib <yesomarib1@gmail.com>
2022-11-25 15:25:44 +0000

    Add an array with the first project object

---
+++ loadProjectAndPopup.js
@@ -0,0 +1,16 @@
+const projectArray = [
+    {
+        ID: 'display-project1',
+        Name: 'Multi-Post Story 1',
+        Description:
+            ' Porject 1 Description: A daily selection of privately personalized reads'
+            + ';'
+            + ' no accounts or sign-ups required.'
+            + "has been the industry's standard dummy text ever since the 1500s,"
+            + 'when an unknown printer took a standard dummy text.',
+        Featured_image: 'image/Img-Placeholder.png',
+        Technologies: ['CSS', 'html', 'bootstrap', 'Ruby'],
+        See_Live: 'https://somarib.github.io/',
+        See_Source: 'https://github.com/somarib/Soma-wk2-portfolio',
+      },
+];
