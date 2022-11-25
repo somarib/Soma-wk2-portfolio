d1569bc13a6f8b3ba583c2d309dad70e38cb5478
tobuya <86710540+tobuya@users.noreply.github.com>
2022-11-25 18:32:35 +0300

    Add projects objects to the project array

--- loadProjectAndPopup.js
+++ loadProjectAndPopup.js
@@ -13,4 +13,46 @@ const projectArray = [
         See_Live: 'https://somarib.github.io/',
         See_Source: 'https://github.com/somarib/Soma-wk2-portfolio',
       },
+    {
+    ID: 'display-project2',
+    Name: 'Multi-Post Story 2',
+    Description:
+        ' Porject 2 Description: A daily selection of privately personalized reads'
+        + ';'
+        + ' no accounts or sign-ups required.'
+        + "has been the industry's standard dummy text ever since the 1500s,"
+        + 'when an unknown printer took a standard dummy text.',
+    Featured_image: 'image/Img-Placeholder.png',
+    Technologies: ['CSS', 'html', 'bootstrap', 'Ruby'],
+    See_Live: 'https://somarib.github.io/',
+    See_Source: 'https://github.com/somarib/Soma-wk2-portfolio',
+  },
+    {
+    ID: 'display-project3',
+    Name: 'Multi-Post Story 3',
+    Description:
+        ' Porject 3 Description: A daily selection of privately personalized reads'
+        + ';'
+        + ' no accounts or sign-ups required.'
+        + "has been the industry's standard dummy text ever since the 1500s,"
+        + 'when an unknown printer took a standard dummy text.',
+    Featured_image: 'image/Img-Placeholder.png',
+    Technologies: ['CSS', 'html', 'bootstrap', 'Ruby'],
+    See_Live: 'https://somarib.github.io/',
+    See_Source: 'https://github.com/somarib/Soma-wk2-portfolio',
+  },
+    {
+    ID: 'display-project4',
+    Name: 'Multi-Post Story 4',
+    Description:
+        ' Porject 4 Description: A daily selection of privately personalized reads'
+        + ';'
+        + ' no accounts or sign-ups required.'
+        + "has been the industry's standard dummy text ever since the 1500s,"
+        + 'when an unknown printer took a standard dummy text.',
+    Featured_image: 'image/Img-Placeholder.png',
+    Technologies: ['CSS', 'html', 'bootstrap', 'Ruby'],
+    See_Live: 'https://somarib.github.io/',
+    See_Source: 'https://github.com/somarib/Soma-wk2-portfolio',
+  },
 ];
