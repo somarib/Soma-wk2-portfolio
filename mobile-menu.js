046ebf5db4059f94d2ef50e68c9120654181ad48
somarib <91262542+somarib@users.noreply.github.com>
2022-11-26 19:29:52 +0000

    Add bugs

--- mobile-menu.js
+++ mobile-menu.js
@@ -1,11 +1,11 @@
-const btn = document.querySelector('#mbm-icon');
+const bbtn = document.querySelector('#mbm-icon');
 
 btn.addEventListener('click', () => {
   document.getElementById('hide-show').style.display = 'block';
 });
 
 document.querySelectorAll('.mbmenu-li-a').forEach((item) => {
-  item.addEventListener('click', () => {
+  item.addEventListener('click',function() {
     document.getElementById('hide-show').style.display = 'none';
   });
 });
