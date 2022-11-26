28d16f02a93dad3f63d5fa83762ba6cae17e154f
tobuya <86710540+tobuya@users.noreply.github.com>
2022-11-26 17:16:40 +0300

    Add form submit event listener

--- validationAndData.js
+++ validationAndData.js
@@ -1,5 +1,6 @@
 const emailValidation = () => {
     const email = document.getElementById("email").value;
     const regExp = /^([a-z\d-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+    document.forms[0].onsubmit = function (e){}
 
 }
