c2ff52ae44fd27058daa90a797eb4d66b2cd6521
somarib <yesomarib1@gmail.com>
2022-11-26 14:29:41 +0000

    Add  a test logic to validationAndData.js file to validate email input

--- validationAndData.js
+++ validationAndData.js
@@ -1,6 +1,16 @@
 const emailValidation = () => {
     const email = document.getElementById("email").value;
     const regExp = /^([a-z\d-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
-    document.forms[0].onsubmit = function (e){}
+    document.forms[0].onsubmit = function (e){
+        if (regularExp.test(email)) {
+            document.getElementById('error-message').innerHTML = '';
+            document.getElementById('error-message').style.display = 'none';
+          } else {
+            document.getElementById('error-message').innerHTML = 'Please enter the email in lower case';
+            document.getElementById('error-message').style.color = 'red';
+            document.getElementById('error-message').style.display = 'block';
+            e.preventDefault();
+          }
+    }
 
 }
