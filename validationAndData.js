80ec1080d70dc7a538bc9311dcca49658e447cc2
somarib <yesomarib1@gmail.com>
2022-11-26 15:17:47 +0000

    Add update to remove linters error

--- validationAndData.js
+++ validationAndData.js
@@ -1,16 +1,17 @@
 const emailValidation = () => {
-    const email = document.getElementById("email").value;
-    const regExp = /^([a-z\d-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
-    document.forms[0].onsubmit = function (e){
-        if (regularExp.test(email)) {
-            document.getElementById('error-message').innerHTML = '';
-            document.getElementById('error-message').style.display = 'none';
-          } else {
-            document.getElementById('error-message').innerHTML = 'Please enter the email in lower case';
-            document.getElementById('error-message').style.color = 'red';
-            document.getElementById('error-message').style.display = 'block';
-            e.preventDefault();
-          }
+  const eemail = document.getElementById('email').value;
+  const regExp = /^([a-z\d-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+  document.forms[0].onsubmit = (e) => {
+    if (regExp.test(eemail)) {
+      document.getElementById('error-message').innerHTML = '';
+    } else {
+      alert(regExp.test(eemail));
+      document.getElementById('error-message').innerHTML = 'Please enter the email in lower case';
+      document.getElementById('error-message').style.color = 'red';
+      document.getElementById('error-message').style.display = 'block';
+      e.preventDefault();
     }
+  };
+};
 
-}
+document.addEventListener('click', emailValidation());
